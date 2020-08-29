62a59d2ffec88075ce9c703068443dbe9499380b
Kenneth Fechter <kenneth.fechter@gmail.com>
2020-08-29 03:19:10 -0400

    Initial Commit

---
+++ Acheron/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Acheron
+//
+//  Created by Kenneth Fechter on 8/29/20.
+//  Copyright © 2020 Kenneth Fechter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
